2d9b333f5da8c833e7b1f187d5b3c02a33ae4b0e
Shay Naeh <shayn@cloudify.co>
2017-12-23 11:37:09 +0000

    helm plugin

---
+++ helminstall.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+kubectl create namespace onap-$2
+kubectl create clusterrolebinding onap-$2-admin-binding --clusterrole=cluster-admin --serviceaccount=onap-$2:default
+kubectl --namespace onap-$2 create secret docker-registry onap-docker-registry-key --docker-server=nexus3.onap.org:10001 --docker-username=docker --docker-password=docker --docker-email=@
+helm install ..//$2/ --name onap-$2 --namespace onap --set nsPrefix=onap,nodePortPrefix=302
